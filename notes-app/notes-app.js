41553b1898a3cfba95686995c9d78de40780aa9e
mannyrhythms <70455477+mannyrhythms@users.noreply.github.com>
2021-03-26 13:29:01 -0400

    50. DOM manipulation

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,12 @@
-console.log('This is from a different file')
+//DOM
+
+// const p = document.querySelector('p')
+// p.remove()
+
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function (p) {
+    p.textContent = '*****'
+    //console.log(p.textContent)
+   // p.remove()
+})
