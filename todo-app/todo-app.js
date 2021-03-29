f5d4d31c7d55b8e313dd82fea7cae54166682313
mannyrhythms <70455477+mannyrhythms@users.noreply.github.com>
2021-03-28 21:36:22 -0400

    52. Challenge

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,20 @@
-const paragraphs = document.querySelectorAll("p")
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
 
-paragraphs.forEach(function (paragraph) {
-    if (paragraph.textContent.includes("the")) {
-        paragraph.remove()
-    }
-})
+// You have 2 todos left - put this in a paragraph tag (p element)
+// Add a p for each todo above (use text value)
+
