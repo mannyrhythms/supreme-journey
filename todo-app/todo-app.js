01343af4f7003f0d61aa4436d2362c203ccdb089
mannyrhythms <70455477+mannyrhythms@users.noreply.github.com>
2021-04-17 22:09:08 -0400

    58. Working with forms

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -5,7 +5,7 @@ const todos = [{
     text: 'Clean kitchen',
     completed: true
 }, {
-    text: 'Buy food',
+    text: 'Cook food',
     completed: true
 }, {
     text: 'Do work',
@@ -15,26 +15,51 @@ const todos = [{
     completed: true
 }]
 
-const incompleteTodos = todos.filter(function (todo) {
-return !todo.completed
-})
 
-const summary = document.createElement('h2')
-summary.textContent =`You have ${incompleteTodos.length} todos left`
-document.querySelector('body').appendChild(summary)
+const filters = {
+    searchText: ''
+}
 
-todos.forEach(function (todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    document.querySelector('body').appendChild(p)
-})
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+        })
 
-//listen for new todo creation
-document.querySelector('#add-todo').addEventListener("click", function (e) {
-console.log('Add a new todo...')
-})
+        document.querySelector('#todos').innerHTML = ''
+        
+        const summary = document.createElement('h2')
+        summary.textContent =`You have ${incompleteTodos.length} todos left`
+        document.querySelector('#todos').appendChild(summary)
+        
+        filteredTodos.forEach(function (todo) {
+            const p = document.createElement('p')
+            p.textContent = todo.text
+            document.querySelector('#todos').appendChild(p)
+        })
+}
 
+renderTodos(todos, filters)
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
-    console.log(e.target.value)
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
+
+//create a form with a single input for todo text
+//setup a submit handler and cancel the default action
+//add a new item to the todo array with that text data (completed value of false)
+//Rerender the application
+//clear the input field value
+
+document.querySelector("#new-todo").addEventListener("submit", function (e) {
+    e.preventDefault()
+    todos.push ({
+        text: e.target.elements.text.value,
+        completed: false
+    })
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
 })
