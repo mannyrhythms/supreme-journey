7f7412b3c72e61905dcd864ae03d6a9a263916ca
mannyrhythms <70455477+mannyrhythms@users.noreply.github.com>
2021-04-23 22:47:22 -0400

    60. Dropdowns

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -18,7 +18,7 @@ const renderNotes = function (notes, filters) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
     })
 
-    document.querySelector('#notes').innerHTML = '<p>TEST</p>'
+    document.querySelector('#notes').innerHTML = " "
 
     filteredNotes.forEach(function (note) {
         const noteEl = document.createElement('p')
@@ -38,7 +38,6 @@ document.querySelector("#search-text").addEventListener("input", function (e) {
     renderNotes(notes, filters)
 })
 
-document.querySelector("#for-fun").addEventListener("change", function (e) {
-console.log(e.target.checked)
+document.querySelector("#filter-by").addEventListener("change", function (e) {
+    console.log(e.target.value)
 })
-
